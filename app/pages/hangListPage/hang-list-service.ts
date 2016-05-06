90ad36b1c481e6b77e11181b6dfb2253790d60e3
Brian Olore <brian.olore@adp.com>
2016-05-06 14:53:28 -0600

    add 'created by' and a bunch more hangs to ui

--- app/pages/hangListPage/hang-list-service.ts
+++ app/pages/hangListPage/hang-list-service.ts
@@ -31,6 +31,9 @@ export default class HangListService {
     return [
       new Hang(me,  [kurt, eric], new Date(), new Date(), 'Bowling on Route 35'),
       h1,
+      h1,
+      h1,
+      h1,
     ];
   }
 
@@ -40,10 +43,14 @@ export default class HangListService {
     let eric = new Person('Eric', 'Stolten');
     let bill = new Person('Bill', 'Croo');
 
-    var h1 = new Hang(me,  [eric, bill], new Date(), new Date(), 'Angular conference lunch');
+    var h1 = new Hang(kurt,  [eric, bill], new Date(), new Date(), 'Angular conference lunch across the street next to the Taco Bell, about a mile away ');
     h1.accepted = true;
 
     return [
+      h1,
+      new Hang(me,  [bill, kurt], new Date(), new Date(), 'Taxi back to airport'),
+      h1,
+      new Hang(me,  [bill, kurt], new Date(), new Date(), 'Taxi back to airport'),
       h1,
       new Hang(me,  [bill, kurt], new Date(), new Date(), 'Taxi back to airport'),
     ];
